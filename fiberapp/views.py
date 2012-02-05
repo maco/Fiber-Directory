3ab0f044e5bfa4d3cd4dd1e005c0c6fb9cae0b80
maco <maco.m@ubuntu.com>
2012-02-05 22:21:58 +0000

    Add knit/crochet/sewing as garment makers with garment filter sidebar

--- fiberapp/views.py
+++ fiberapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from fiberapp.models import Services
 from fiberapp.models import Farming
 from fiberapp.models import Source
+from fiberapp.models import Garments
 from django.views.generic.base import TemplateView
 # Create your views here.
 
@@ -28,14 +29,25 @@ class FarmingView(TemplateView):
         for field in Farming._meta.many_to_many:
             print field.name
             farming.append(field)
+        return {'farming_list':farming,'services_list':services(),'artisan_list':Source.objects.filter(services__farming=1)}
 
-        farmers = []
-
-        return {'farming_list':farming,'services_list':services(),'farmer_list':farmers}
+class FabricView(TemplateView):
+    template_name = 'fabric.html'
+    def get_context_data(self, **kwargs):
+        garments = []
+        for field in Garments._meta.fields:
+            if not field.name is 'id':
+                garments.append(field)
+        keyword = 'services__'+self.args[0]
+        artisans = Source.objects.filter(**{keyword:1})
+        return {'garment_list':garments,'services_list':services(),'artisan_list':artisans,'fabric_type':self.args[0]}
 
 class BasicView(TemplateView):
     template_name = 'basic.html'
     def get_context_data(self, **kwargs):
-        keyword = 'services__'+self.args[0]
-        artisans = Source.objects.filter(**{keyword:1})
-        return {'artisan_list':artisans,'services_list':services()}
+        if self.args[0] == "all":
+            return {'artisan_list':Source.objects.all(),'services_list':services()}
+        else: 
+            keyword = 'services__'+self.args[0]
+            artisans = Source.objects.filter(**{keyword:1})
+            return {'artisan_list':artisans,'services_list':services()}
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from fiberapp.views import HomeView
 from fiberapp.views import FarmingView
+from fiberapp.views import FabricView
 from fiberapp.views import BasicView
 
 # Uncomment the next two lines to enable the admin:
@@ -10,6 +11,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$',HomeView.as_view(), name='home'),
     url(r'^service/farming/$',FarmingView.as_view(), name='farming'),
+    url(r'^service/(knitting|crochet|sewing)/$',FabricView.as_view(),name='fabric'),
     url(r'^service/(\w+)/$',BasicView.as_view(), name='basic'),
     # url(r'^fiber/', include('fiber.foo.urls')),
 
