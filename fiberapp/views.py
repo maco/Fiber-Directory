29996927d929f754a1329490c0e1d0042e713130
maco <maco.m@ubuntu.com>
2012-02-06 02:05:04 +0000

    Make farming drill-down work, add contact/about/etc type pages

--- fiberapp/views.py
+++ fiberapp/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from fiberapp.models import Services
-from fiberapp.models import Farming
-from fiberapp.models import Source
-from fiberapp.models import Garments
+from fiberapp.models import Source, Services, Farming, Garments
+from fiberapp.models import Sheep_Breeds, Alpaca_Breeds, Rabbit_Breeds, Goat_Breeds, Plants
 from django.views.generic.base import TemplateView
+from django.template.defaultfilters import slugify
 # Create your views here.
 
 
@@ -22,14 +21,40 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         return {'services_list':services()}
 
+class JunkView(TemplateView):
+    template_name='junk.html'
+    def get_context_data(self, **kwargs):
+        if len(self.args) == 1:
+            return {'services_list':services(),'junk':self.args[0]}
+        return {'services_list':services()}
+
 class FarmingView(TemplateView):
     template_name = 'farming.html'
+    tables = {'wool':'Sheep_Breeds',
+                'alpaca':'Alpaca_Breeds',
+                'rabbit':'Rabbit_Breeds',
+                'goat':'Goat_Breeds',
+                'plants':'Plants'}
     def get_context_data(self, **kwargs):
-        farming = []
+        artisans = Source.objects.filter(services__farming__isnull=False)
+        # make a filter list for sidebar
+        sidebar = []
         for field in Farming._meta.many_to_many:
-            print field.name
-            farming.append(field)
-        return {'farming_list':farming,'services_list':services(),'artisan_list':Source.objects.filter(services__farming=1)}
+            sidebar.append(field)
+        second_level=""
+        if len(self.args) == 1:
+            keyword = "services__farming__"+slugify(self.args[0])+"__isnull"
+            artisans = Source.objects.filter(**{keyword:False})
+            sidebar = []
+            for breed in eval('%s._meta.fields' % self.tables[self.args[0]]):
+                if not breed.name is 'id':
+                    sidebar.append(breed)
+            second_level=slugify(self.args[0])
+        elif len(self.args) == 2:
+            keyword = "services__farming__"+slugify(self.args[0])+"__"+slugify(self.args[1])
+            artisans = Source.objects.filter(**{keyword:True})
+
+        return {'sidebar_list':sidebar,'services_list':services(),'artisan_list':artisans,'second_level':second_level}
 
 class FabricView(TemplateView):
     template_name = 'fabric.html'
@@ -38,9 +63,9 @@ class FabricView(TemplateView):
         for field in Garments._meta.fields:
             if not field.name is 'id':
                 garments.append(field)
-        keyword = 'services__'+self.args[0]
-        artisans = Source.objects.filter(**{keyword:1})
-        return {'garment_list':garments,'services_list':services(),'artisan_list':artisans,'fabric_type':self.args[0]}
+        keyword = 'services__'+slugify(self.args[0])+"__isnull"
+        artisans = Source.objects.filter(**{keyword:False})
+        return {'sidebar_list':garments,'services_list':services(),'artisan_list':artisans,'fabric_type':slugify(self.args[0])}
 
 class BasicView(TemplateView):
     template_name = 'basic.html'
@@ -48,6 +73,6 @@ class BasicView(TemplateView):
         if self.args[0] == "all":
             return {'artisan_list':Source.objects.all(),'services_list':services()}
         else: 
-            keyword = 'services__'+self.args[0]
-            artisans = Source.objects.filter(**{keyword:1})
+            keyword = 'services__'+slugify(self.args[0])
+            artisans = Source.objects.filter(**{keyword:True})
             return {'artisan_list':artisans,'services_list':services()}
--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from fiberapp.views import HomeView
 from fiberapp.views import FarmingView
 from fiberapp.views import FabricView
 from fiberapp.views import BasicView
+from fiberapp.views import JunkView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,9 +12,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$',HomeView.as_view(), name='home'),
     url(r'^service/farming/$',FarmingView.as_view(), name='farming'),
+    url(r'^service/farming/(\w+)/$',FarmingView.as_view(), name='farming'),
+    url(r'^service/farming/(\w+)/(\w+)/$',FarmingView.as_view(), name='farming'),
     url(r'^service/(knitting|crochet|sewing)/$',FabricView.as_view(),name='fabric'),
     url(r'^service/(\w+)/$',BasicView.as_view(), name='basic'),
-    # url(r'^fiber/', include('fiber.foo.urls')),
+    url(r'^(\w+)$',JunkView.as_view(),name='contact'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
